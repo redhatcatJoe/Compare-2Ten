344b96c03b99942bce77ad7178c59872394a19fe
redhatcatJoe <redhatcatjoe@gmail.com>
2019-10-02 16:31:41 +0500

    first version

--- src/Comparator.java
+++ src/Comparator.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 public class Comparator {
 
 
-    String compare(String a, String b){
+    String compare(int a, int b){
         String r ="nothing";
-        int a1 = Integer.parseInt (a) - 10;
-        int b1= Integer.parseInt (b) - 10;
-        a1 = abc (a1);        //positive numberrr
-        b1 = abc (b1);       //
-        if (a1 >b1)
-            r = "b close to 10";
-        if (a1==b1) {
-
-            r = "a equal b";
+      int  a1=abc(10-a);
+       int b1= abc( 10-b);
+               //   2              9   1
+        if( a1 > b1){
+            r = b+" close to 10";
         }
-        else r = "a close to 10";
+        else  if (a1==b1){
+            r= "a equals b";
+        }
+
+
+        else r = a+" close to 10";
 
         return r;
 
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ Comparator comparator = new Comparator ();
         Scanner s = new Scanner (System.in);
         System.out.println ("enter a  and b" );
 
-        System.out.println (comparator.compare (s.nextLine (), s.nextLine ()) );
+        System.out.println (comparator.compare (s.nextInt (), s.nextInt ()) );
     }
 }
